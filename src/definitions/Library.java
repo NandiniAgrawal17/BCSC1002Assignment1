c58f6e76241582a77165c1ff6be175f55cd24cf3
NandiniAgrawal17 <nandini.agrawal_cs19@gla.ac.in>
2020-08-30 11:05:30 +0900

    create a field for Library.java class.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,27 +7,28 @@
 package definitions;
 
 public class Library {
-    private Book[] store; // field
+    // field
+    private Book[] store;
 
 
-    //constructor,at least 1
-    public Library() {
-        this.store = new Book[5];
-        for (int i = 0; i < store.length; i++) {
-            store[i] = new Book();
-
-        }
-    }
-
-    //getter
-
-    public Book[] getStore() {
-        return store.clone(); //.clone() will return a copy of array ,
-        //no the array reference itself.
-    }
-
-    //setter
-    public void setStore(Book[] store) {
-        this.store = store;
-    }
-}
+//    //constructor,at least 1
+//    public Library() {
+//        this.store = new Book[5];
+//        for (int i = 0; i < store.length; i++) {
+//            store[i] = new Book();
+//
+//        }
+//    }
+//
+//    //getter
+//
+//    public Book[] getStore() {
+//        return store.clone(); //.clone() will return a copy of array ,
+//        //no the array reference itself.
+//    }
+//
+//    //setter
+//    public void setStore(Book[] store) {
+//        this.store = store;
+//    }
+//}
